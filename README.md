eea167fd4b99e21a7fcd41f0f81a8708bccef6f3
Indigo <j.c.f.gauss@sjtu.edu.cn>
2021-10-20 15:50:53 +0800

    structures

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # docker-ocr-api
+
+```bash
+$ docker build -f Dockerfile -t qhduan/pdocr-api:0.1 .
+
+$ docker run -it --rm -p 8000:8000 qhduan/pdocr-api:0.1
+```
---
+++ main.py
@@ -0,0 +1,26 @@
+from paddleocr import PaddleOCR
+from fastapi import FastAPI
+from paddleocr.paddleocr import main
+import uvicorn
+ocr = PaddleOCR(use_angle_cls=True, lang="ch")
+
+
+app = FastAPI()
+
+
+@app.get('/')
+async def root():
+    return {1: 1}
+
+
+@app.post('/api/ocr/{imgpath}')
+async def get_orc(imgpath: str):
+    results = ocr.ocr(imgpath, cls=True)
+    text = [r[1][0] for r in results]
+    return {
+        'ok': True,
+        'data': text
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True, debug=True)
