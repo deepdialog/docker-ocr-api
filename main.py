03916090c8c9e955ebb8ff4b357ad3506b9cccf6
Indigo <j.c.f.gauss@sjtu.edu.cn>
2021-11-18 13:50:26 +0800

    cpu occupation

--- main.py
+++ main.py
@@ -45,4 +45,5 @@ async def get_orc(request: Request):
     }
 
 if __name__ == "__main__":
-    uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True, debug=True)
+    # uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True, debug=True)
+    uvicorn.run(app="main:app", host="0.0.0.0", port=8000)
--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 python main.py
+# uvicorn main:app --host 0.0.0.0 --port 8000
